26fdd35d93095cec32b6eeb538aadef96eff3416
Steven Luu <sluu99@gmail.com>
2013-03-10 16:01:02 -0500

    Added SQL scripts for postgres implementation

---
+++ sql/pg_tables.sql
@@ -0,0 +1,20 @@
+create sequence um_users_id_seq start 1;
+
+create table um_users (
+	id bigint not null default nextval('um_users_id_seq'),
+	user_name varchar(64) not null,
+	email_addr varchar(128) not null default '',
+	status integer not null default 0,
+	hash varchar(128) not null default '',
+	salt varchar(32) not null default '',
+	created_on timestamp with time zone not null default 'now',
+	last_login timestamp with time zone not null default 'epoch',
+
+	constraint pk_um_users primary key(id),
+	constraint ux_um_users_username unique(user_name)
+);
+
+create index ix_um_users_email_addr on um_users(email_addr);
+create index ix_um_users_created_on on um_users(created_on);
+create index ix_um_users_last_login on um_users(last_login);
+
