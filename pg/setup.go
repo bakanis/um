cc4471f99d43deca46e6cadf9559a19c23a2decf
Steven Luu <sluu99@gmail.com>
2013-03-10 16:34:39 -0500

    Implemented setup

--- pg/setup.go
+++ pg/setup.go
@@ -1,5 +1,18 @@
 package pg
 
+import "database/sql"
+
+// Setup prepares the manager's database connections
 func (this *Manager) Setup(dns string) error {
+	var err error
+	this.session, err = sql.Open("postgres", dns)
+	return err
+}
+
+// Close cleans up the database connection of the current manager
+func (this *Manager) Close() error {
+	if this.session != nil {
+		return this.session.Close()
+	}
 	return nil
 }
