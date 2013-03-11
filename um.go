2af01b29c4a72247c4980dc70bd8371870fce425
Steven Luu <sluu99@gmail.com>
2013-03-11 16:50:10 -0500

    Updated module variable names

--- um.go
+++ um.go
@@ -2,23 +2,23 @@ package um
 
 import "fmt"
 
-var managers = make(map[string]UserManager)
+var gManagers = make(map[string]UserManager)
 
 // Register registers a user manager
 func Register(name string, manager UserManager) {
 	if name == "" {
 		panic("um: Manager name cannot be empty")
 	}
-	if _, exists := managers[name]; exists == true {
+	if _, exists := gManagers[name]; exists {
 		panic(fmt.Sprintf("um: Manager named '%s' already exists", name))
 	}
-	managers[name] = manager
+	gManagers[name] = manager
 }
 
 // Open opens a user manager session
 func Open(name, dns string) (UserManager, error) {
-	manager, exists := managers[name]
-	if exists == false {
+	manager, exists := gManagers[name]
+	if !exists {
 		panic(fmt.Sprintf("um: Manager named '%s' does not exist", name))
 	}
 	err := manager.Setup(dns) // setup manager
